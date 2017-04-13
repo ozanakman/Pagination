3b9383b9a0c320027bf2e18876b3ac7c6d1d0a10
Ozan Akman <info@ozanakman.com.tr>
2017-04-14 01:45:42 +0300

    Update Pagination.php

--- src/Pagination.php
+++ src/Pagination.php
@@ -1,15 +1,7 @@
 <?php
 /**
- * Easy Pagination Class
- *
- * This class written for building advanced and beautiful pagination output.
+ * This class has written for building advanced and beautiful pagination output.
  * You can easily build your structure with just 2 lines.
- *
- * For Example:
- *
- * $pagination	= new \Sythdev\Pagination(100, 1, 20, 1, 3);
- * $output		= $pagination->build('somelink/page/','ul','li','ul-class','li-class', 'active');
- * echo $output;
  *
  * @package     Easy Pagination
  * @author      Ozan "sythdev" Akman <info@ozanakman.com.tr>
@@ -17,7 +9,7 @@
  *
  */
 
-namespace Sythdev;
+namespace Akman;
 
 class Pagination{
 
